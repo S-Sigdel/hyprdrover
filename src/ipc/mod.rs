8453da02aa8ecb248f03c4a7ab3e55c91cf1cb57
S-Sigdel <sakshyamsigdel@gmail.com>
2025-12-20 16:27:31 -0600

    restore: rebuild tiling order via split-tree + dwindle preselect

--- src/ipc/mod.rs
+++ src/ipc/mod.rs
@@ -2,6 +2,6 @@ pub mod hypr_commands;
 
 // Re-export the actual functions and structs we created
 pub use hypr_commands::{
-    capture_state, dispatch, move_window_pixel, move_window_to_workspace, resize_window_pixel,
-    HyprClient, SessionSnapshot,
+    capture_state, dispatch, get_active_workspace, move_window_pixel, move_window_to_workspace,
+    resize_window_pixel, HyprClient, SessionSnapshot,
 };
